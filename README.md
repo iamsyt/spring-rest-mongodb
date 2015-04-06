9489449ce41b60ad73efbb7a04afb2b6c957bf78
rohitrajt <rohitrajt@gmail.com>
2015-04-05 23:26:19 -0700

    Changed deployments to support both embedded tomcat and standalone tomcat configurations
    Added Readme.md to run the applications

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 
 1. Java 7+ (I used Java 1.8.0_25)
 2. MongoDB [https://www.mongodb.org/downloads](https://www.mongodb.org/downloads)
+3. Tomcat 7,8
+
+## Steps to run
+
+1. Using embedded Tomcat
+
+..* Build the war file using this command from the project root `mvn clean package`
+..* Run the war file using this command from the project root `java -jar target/springRestMongo.war`
+
+2. Using standalone Tomcat
+
+..* Build the war file using this command from the project root `mvn clean package`
+..* Copy the war file into your tomcats webapps folder and start tomcat.
--- src/main/groovy/spring/rest/mongodb/Application.groovy
+++ src/main/groovy/spring/rest/mongodb/Application.groovy
@@ -1,15 +1,22 @@
 package spring.rest.mongodb
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.context.annotation.ImportResource
 
-
-@EnableAutoConfiguration
+@SpringBootApplication
 @ImportResource("classpath:resources.xml")
-class Application {
+class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+        return application
     }
 }
